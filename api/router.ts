1b335aa94366f8c48aaf56393ac60309221f202f
kiwikoru <kiwikoru3d@gmail.com>
2026-05-31 17:33:31 +1200

    router.ts

--- api/router.ts
+++ api/router.ts
@@ -1,5 +1,5 @@
-import { createRouter, publicQuery } from "./middleware";
-import { emailRouter } from "./email";
+import { createRouter, publicQuery } from "./middleware.js";
+import { emailRouter } from "./email.js";
 
 export const appRouter = createRouter({
   ping: publicQuery.query(() => ({ ok: true, ts: Date.now() })),
