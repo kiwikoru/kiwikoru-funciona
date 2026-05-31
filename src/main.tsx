9556a77b49423e30dc1bb9d8467cba381c66afde
kiwikoru <kiwikoru3d@gmail.com>
2026-05-31 18:55:42 +1200

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,15 @@ import { HelmetProvider } from 'react-helmet-async'
 import { TRPCProvider } from '@/providers/trpc'
 import './index.css'
 import App from './App'
-import ScrollToTop from './components/ScrollToTop'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <TRPCProvider>
-<HashRouter>
-  <ScrollToTop />
-  <App />
-</HashRouter>
+        <HashRouter>
+          <ScrollToTop />
+          <App />
+        </HashRouter>
       </TRPCProvider>
     </HelmetProvider>
   </StrictMode>,
