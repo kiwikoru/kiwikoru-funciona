631f88fe2f2a07e7fa1796a490d4deb6d6d8e1bc
kiwikoru <kiwikoru3d@gmail.com>
2026-06-12 18:54:43 +1200

    Add files via upload

--- api/router.ts
+++ api/router.ts
@@ -1,11 +1,9 @@
 import { createRouter, publicQuery } from "./middleware";
 import { enquiryRouter } from "./routers/enquiry";
-import { emailRouter } from "./routers/email";
 
 export const appRouter = createRouter({
   ping: publicQuery.query(() => ({ ok: true, ts: Date.now() })),
   enquiry: enquiryRouter,
-  email: emailRouter,
 });
 
 export type AppRouter = typeof appRouter;
