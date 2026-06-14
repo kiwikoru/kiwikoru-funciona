fee3b9738d9cb4337479312d09f7f4ca5ef49244
kiwikoru <kiwikoru3d@gmail.com>
2026-06-14 14:03:05 +1200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,29 +8,38 @@ import Services from './pages/Services'
 import Materials from './pages/Materials'
 import Quote from './pages/Quote'
 import Contact from './pages/Contact'
+import { QuoteProvider } from './contexts/QuoteContext'
 
 function ScrollToTop() {
   const { pathname } = useLocation()
-  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return null
 }
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-white">
-      <Navbar />
-      <ScrollToTop />
-      <main id="main-content" className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/materials" element={<Materials />} />
-          <Route path="/quote" element={<Quote />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </main>
-      <Footer />
-      <WhatsAppFloat />
-    </div>
+    <QuoteProvider>
+      <div className="min-h-screen flex flex-col bg-white">
+        <Navbar />
+        <ScrollToTop />
+
+        <main id="main-content" className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/materials" element={<Materials />} />
+            <Route path="/quote" element={<Quote />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+
+        <Footer />
+        <WhatsAppFloat />
+      </div>
+    </QuoteProvider>
   )
 }
